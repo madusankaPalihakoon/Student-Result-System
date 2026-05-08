9eec8dfbf4837e0871ba3964e55b1fafb296805a
Tharushi16 <sis24b110@uovt.ac.lk>
2026-05-08 20:32:30 +0530

    Update api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -26,6 +26,7 @@ header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Credentials: true");
 
 
+
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit();
